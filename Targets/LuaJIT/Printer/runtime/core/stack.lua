1797de1ce55fe432ec004ce466c0b6a9dfafac43
SovereignSatellite <suillus_helenae@pm.me>
2026-04-27 15:22:26 -0400

    Replace linear stack with sized segments

--- Targets/LuaJIT/Printer/runtime/core/stack.lua
+++ Targets/LuaJIT/Printer/runtime/core/stack.lua
@@ -1,2 +1,33 @@
--- SECTION excess_stack
-local excess_stack = { top = 0 }
+-- SECTION stack_pool
+local stack_pool = {
+	[16] = {},
+	[64] = {},
+	[256] = {},
+	[1024] = {},
+	[4096] = {},
+	[16384] = {},
+}
+
+-- SECTION stack_acquire
+-- NEEDS stack_pool
+require("table.new")
+
+local function stack_acquire(size_class)
+	local stack = table.remove(stack_pool[size_class])
+
+	if stack == nil then
+		return table.new(size_class, 0)
+	else
+		return stack
+	end
+end
+
+-- SECTION stack_release
+-- NEEDS stack_pool
+require("table.clear")
+
+local function stack_release(size_class, stack)
+	table.clear(stack)
+
+	table.insert(stack_pool[size_class], stack)
+end
